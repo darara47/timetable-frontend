5c27db6c0dda8f2730aea7939ebd1e45f3be2505
Patryk Dulniok <patrykdulniok@op.pl>
2023-03-17 10:25:04 +0100

    Create service for timetable

--- src/screens/Timetable/Timetable.tsx
+++ src/screens/Timetable/Timetable.tsx
@@ -1,10 +1,34 @@
-import { ReactElement } from 'react';
+import { ReactElement, useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
+import { getLessonsBySectionId } from '../../services/lessonsService';
+import { Lesson } from '../../types/lessons.types';
+import { StackNavigation } from '../../types/stackParamList';
 
-const TimetableScreen = (): ReactElement => {
+type TimetableScreenProps = {
+  navigation: StackNavigation,
+  route: {
+    params: {
+      sectionId: string,
+    },
+  },
+}
+
+const TimetableScreen = (timetableScreenProps: TimetableScreenProps): ReactElement => {
+  const { navigation, route: { params: { sectionId } } } = timetableScreenProps;
+  const [timetable, setTimetable] = useState<Lesson[]>();
+
+  useEffect(() => {
+    const getLessons = async () => {
+      const lessons = await getLessonsBySectionId(sectionId);
+      setTimetable(lessons);
+    }
+    getLessons();
+  }, []);
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Timetable Screen</Text>
+      <Text>{sectionId}</Text>
     </View>
   );
 }
